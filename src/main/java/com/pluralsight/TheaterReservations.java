567eb97bb1a96d7bcbac7f050d48204298ed85cd
josepagan <josepaganrnj@outlook.com>
2026-04-15 12:44:37 -0400

    Initial Commit, rough code layout

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
 public class TheaterReservations {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        System.out.println("What date is your show?(MM/dd/yyyy)");
+        System.out.println("How many tickets would you need?");
+
+        String userName = "Jose Pagan";
+        String dateOfShow = "01/01/2027";
+        int numberOfTickets = 2;
+
+        String[] split = userName.split("");
+        String firstName = split[1];
+        String lastName = split[2];
+
+        System.out.println(numberOfTickets + "ticket"+ "reserved for " + dateOfShow + " for " + lastName + "," + firstName);
+
+
+    }
+
 }
